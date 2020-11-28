e11e35e74af9363cd88725b2f94f3294e2ba0e0f
readysloth <readysloth@protonmail.com>
2020-11-28 11:44:44 +0300

    add(ffi_reader.rkt): Added reader

---
+++ ffi_reader.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(require string-util)
+(require 2htdp/batch-io)
+(require ffi/unsafe ffi/unsafe/define)
+
+
+(provide read read-syntax)
+
+
+(define (read in)
+  (read-syntax #f in))
+
+
+(define (read-syntax src in) 
+  (define parsed-line (parse-line (read-line in)))
+  (to-syntax (list (string->symbol (first parsed-line))
+                   (first (rest parsed-line)))
+             #f #f))
+
+
+(define (to-syntax v src ln)
+  (datum->syntax #f v #f));(make-srcloc src ln)))
+
+
+(define (parse-line line)
+  (if (starts-with-char? line #\#)
+    (parse-define line)
+    (line)))
+
+
+(define (parse-define line)
+  (rest (regexp-match #px"#(use|header|fregex)\\s+(\\S+)" line)))
